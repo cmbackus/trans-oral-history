1f31a676e8ec00125f580ab192f2d6855d75d070
Charlie Backus <cbackus123@gmail.com>
2016-04-25 23:18:00 -0400

    freaking audio works

--- web/js/carouselToggle.js
+++ web/js/carouselToggle.js
@@ -1,6 +1,13 @@
 var personIds = ["metzler", "bills", "delaney", "barres", "waters", "otoole", "archived"];
 
-
+//carousel audio
+$('#metzlerSlide').on('slide', function (e) {
+    console.log("metzlerSlide clicked");
+    //var xx = $(this);
+    //setTimeout(function () {
+    //    xx.find('.active').next().find('.slab').slabText();
+    // }, 0);
+});
 //glossary carousel
 
 $('.glossaryLink').click(function (e) {
@@ -10,25 +17,21 @@ $('.glossaryLink').click(function (e) {
 
 
 })
-
-
-
-
-
-
-
-
-
-//people carousel
-/*for (var i = 0; i > personIds; i++) {
-    $('#' + personIds[i]).click(function () {
-        console.log(personIds[i] + " clicked")
-        $('#' + personIds[i] + 'Section').removeClass("hideSection")
-        $('#people').addClass("hideSection")
-
+$('.refreshIframe').click(function () {
+        console.log("refreshing iframe")
+        $(this).parents("iframe").html("<iframe width='100%' height='166' scrolling='no' frameborder='no' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/258889676&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;show_artwork=false'></iframe>")
+            //$('iframe').attr('src', $('iframe').attr('src'));
     })
+    //people carousel
+    /*for (var i = 0; i > personIds; i++) {
+        $('#' + personIds[i]).click(function () {
+            console.log(personIds[i] + " clicked")
+            $('#' + personIds[i] + 'Section').removeClass("hideSection")
+            $('#people').addClass("hideSection")
 
-}*/
+        })
+
+    }*/
 
 /*$('#metzler').click(function () {
     console.log("metzler clicked")
@@ -36,7 +39,9 @@ $('.glossaryLink').click(function (e) {
     $('#people').addClass("hideSection")
 
 })*/
+//select sections
 $('#metzler').click(function () {
+    //$('iframe').attr('src', $('iframe').attr('src'));
     console.log("metzler clicked")
     $('#metzlerSection').removeClass("hideSection")
     $('#people').addClass("hideSection")
@@ -85,8 +90,52 @@ $('#archived').click(function () {
 
 })
 $('.personClose').click(function () {
+        console.log("close clicked")
+        $('.PeopleCarouselSection').addClass("hideSection")
+        $('#people').removeClass("hideSection")
+
+    })
+    //topics carousel
+$('.topicClose').click(function () {
     console.log("close clicked")
-    $('.PeopleCarouselSection').addClass("hideSection")
-    $('#people').removeClass("hideSection")
+    $('.TopicCarouselSection').addClass("hideSection")
+    $('#topics').removeClass("hideSection")
+
+})
+
+$('#community').click(function () {
+
+    $('#communitySection').removeClass("hideSection")
+    $('#topics').addClass("hideSection")
+
+})
+$('#workplace').click(function () {
+
+    $('#workplaceSection').removeClass("hideSection")
+    $('#topics').addClass("hideSection")
+
+})
+$('#healthcare').click(function () {
+
+    $('#healthcareSection').removeClass("hideSection")
+    $('#topics').addClass("hideSection")
+
+})
+$('#political').click(function () {
+
+    $('#politicalSection').removeClass("hideSection")
+    $('#topics').addClass("hideSection")
+
+})
+$('#transparenthood').click(function () {
+
+    $('#transparenthoodSection').removeClass("hideSection")
+    $('#topics').addClass("hideSection")
+
+})
+$('#visibility').click(function () {
+
+    $('#visibilitySection').removeClass("hideSection")
+    $('#topics').addClass("hideSection")
 
 })
